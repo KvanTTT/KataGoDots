ffbaf5e8ff1d0bcc2e08f7c2c810c285f42477be
Ivan Kochurkin <kvanttt@gmail.com>
2025-11-30 18:26:15 +0100

    Introduce `Board::getAcceptableKomiRange`

--- cpp/game/dotsfield.cpp
+++ cpp/game/dotsfield.cpp
@@ -949,3 +949,10 @@ void Board::calculateOneMoveCaptureAndBasePositionsForDots(vector<Color>& captur
     }
   }
 }
+
+std::pair<float, float> Board::getAcceptableKomiRange(const bool allowDraw) const {
+  assert(rules.isDots);
+  auto lowerBound = static_cast<float>(-whiteScoreIfBlackGrounds) + (allowDraw ? 0.0f : 0.5f);
+  auto upperBound = static_cast<float>(blackScoreIfWhiteGrounds) + (allowDraw ? 0.0f : -0.5f);
+  return {lowerBound, upperBound };
+}
