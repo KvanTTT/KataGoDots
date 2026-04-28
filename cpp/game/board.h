d1d7140b02cf5ba14b6229eba32cfce660bc3e35
Ivan Kochurkin <kvanttt@gmail.com>
2026-04-28 20:21:20 +0200

    Ignore removed bases when filtering and handling captures and territories. Mark bases as removed during removal to track state consistently.

--- cpp/game/board.h
+++ cpp/game/board.h
@@ -455,6 +455,7 @@ struct Board
     Loc captureLoc{};
     Player player{};
     Base::Type type{};
+    bool removed;
     std::unordered_set<Loc> territory;
 
     BaseInfo(const Base& base, Loc newCaptureLoc);
@@ -468,7 +469,7 @@ struct Board
 
     void addCaptureInfo(BaseInfo* newBaseInfo);
     void addTerritoryInfo(BaseInfo* newBaseInfo);
-    bool removeCaptureAndTerritoryInfos(const BaseInfo* baseInfoToRemove);
+    bool removeCaptureAndTerritoryInfos(BaseInfo* baseInfoToRemove);
 
     [[nodiscard]] Color getOneMoveCaptureColor() const;
     [[nodiscard]] Color getOneMoveEmptyCaptureColor() const;
